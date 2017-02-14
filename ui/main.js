2119dbe96c38ce40e6448649097ff44b401fe9f9
Rajeshsekaran <rajeshsekaran007@gmail.com>
2017-02-14 12:42:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML = 'new value';
 
 //move the image 
 var img = document.getElementById('madi');
+var marginleft = 0;
+function moveRight() {
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + 'px';
+}
 img.onclick = function () {
-    img.style.marginleft = '100px';
+    var interval = setInterval(moveRight, 100);
+ 
 };
